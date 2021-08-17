92c8f078a00cce7214658e489292ef359a44ccff
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-08-18 05:16:13 +0800

    added dynamic post count in metrics endpoint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
@@ -10,19 +11,30 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
     return post
+
+# Function to count the number of posts in the database
+def count_post():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    post_count = cursor.execute('SELECT COUNT() FROM posts').fetchone()
+    connection.close()
+    return post_count[0]
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,22 +42,25 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -57,13 +72,14 @@ def create():
         else:
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+                               (title, content))
             connection.commit()
             connection.close()
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
 
 # Define the Healthcheck endpoint
 @app.route('/healthz')
@@ -75,11 +91,12 @@ def healthz():
     )
     return response
 
+
 # Define the metrics endpoint
 @app.route('/metrics')
 def metrics():
     response = app.response_class(
-        response=json.dumps({"db_connection_count": "$db_count_here", "post_count": "$post_count_here"}),
+        response=json.dumps({"db_connection_count": "$db_count_here", "post_count": count_post()}),
         status=200,
         mimetype='application/json'
     )
@@ -88,5 +105,5 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   # app.run(host='0.0.0.0', port='3111')
-   app.run(host='127.0.0.1', port='3111')
+    # app.run(host='0.0.0.0', port='3111')
+    app.run(host='127.0.0.1', port='3111')
